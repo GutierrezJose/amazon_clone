4857fe3909d0d617ef57ee571831a6da232c7044
GutierrezJose <gutierreztorrezjose@gmail.com>
2024-11-06 21:30:51 -0400

    About Amazon text color was changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CarritoComponent } from './pages/carrito/carrito.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ProductPurchaseComponent } from './pages/product-purchase/product-purchase.component';
+import { ProductsComponent } from './pages/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'about', component: AboutComponent},
+    {path: 'carrito', component: CarritoComponent},
+    {path: '', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'products/:id', component: ProductDetailComponent},
+    {path: 'products/purchase/:id', component: ProductPurchaseComponent},
+    {path: 'products', component: ProductsComponent},
+    {path: '**', component: HomeComponent}
+];
