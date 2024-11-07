4ea30d198aab19dc9bb29a40d4acf8efdc143344
GutierrezJose <gutierreztorrezjose@gmail.com>
2024-11-06 20:52:04 -0400

    Components and Routes were added

---
+++ src/app/pages/product-purchase/product-purchase.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-purchase',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-purchase.component.html',
+  styleUrl: './product-purchase.component.css'
+})
+export class ProductPurchaseComponent {
+
+}
